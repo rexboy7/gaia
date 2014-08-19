57b29cccde77854c9434aa7284bc2b97f7a11ba4
Rex Lee <rexboy@mozilla.com>
2014-08-19 19:34:46 +0800

    Abstract session object for both primary/secondary side use

--- dev_apps/tcpsocket/statics/lib/js/presentation.js
+++ dev_apps/tcpsocket/statics/lib/js/presentation.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var Presentation = {
-  __proto__: PureHTTPPeer,
+  channelPeer: PureHTTPPeer,
   sessions: {},
+
   init: function() {
-    PureHTTPPeer.init.apply(this);
+    this.channelPeer.init();
+    this.channelPeer.onDataChannelOpened = this.onDataChannelOpened.bind(this);
+    this.channelPeer.onDataChannelReceive = this.onDataChannelReceive.bind(this);
   },
   onDataChannelOpened: function pr_onRemoteJoin() {
     this.onavailablechange && this.onavailablechange({available: true});
@@ -14,84 +17,34 @@ var Presentation = {
     if (!url) {
       return;
     }
-    this.sessions[url] = new PresentationSession(url);
+    console.log("REQUESTSESSION");
+    var session = new PresentationSession(PrimarySessionSignaler);
+    session.request(url);
+    this.sessions[url] = session;
     //this.presentataionSession = new PresentationSession(url);
-    return this.sessions[url];
+    return session;
   },
-  /*
-  onDataChannelOpened: function p_onDataChannelOpened(evt) {
-
-     if (this.rank == 'host') {
-      this.presentationSession.currentstate = 'connected';
-      this.presentationSession.onstatechange &&
-        this.presentationSession.onstatechange();
-    } else {
-      this.presentationSession = new PresentationSession();
-      this.presentationSession.currentstate = 'connected';
-      this.onpresent &&
-        this.onpresent({session: this.presentationSession});
-    }
-  },*/
   onDataChannelReceive: function p_onDataChannelReceive(message) {
+    console.log("EXPECT:PRESENTOFFER");
     switch(message.event) {
       case 'presentoffer':
         this.sessions[message.id]._sendAnswer(message.data);
+        break;
     }
-    //this.presentationSession.onmessage &&
-      //this.presentationSession.onmessage(evt.data);
+  },
+  log: function p_log(message) {
+    this.channelPeer.log(message);
   },
   ///////////////////////////////////////////////////////////
   onavailablechange: null,
   onpresent: null
 };
 
-function PresentationSession(url) {
-  Presentation.dataChannelSend('opensession', url);
-  this._url = url;
-  this.id = url;
-  this._currentstate = 'disconnected';
-}
-
-PresentationSession.prototype = {
-  pc: null,
-  get state() {
-    return this._currentstate;
+var PrimarySessionSignaler = {
+  send: function pss_sendMessage(event, data, id) {
+    Presentation.channelPeer.dataChannelSend('presentanswer', data, id);
   },
-  postMessage: function ps_postMessage(message) {
-    this.dc.send(JSON.stringify(message));
-  },
-  close: function ps_close() {
-
-  },
-  _sendAnswer: function ps_sendAnswer(offer) {
-    this.pc = new RTCPeerConnection(pc_config, pc_constraints);
-    this.pc.setRemoteDescription(new RTCSessionDescription(offer), function() {
-      this.pc.createAnswer(function(answer) {
-        this.pc.setLocalDescription(answer);
-        Presentation.dataChannelSend('presentanswer', answer, this.id);
-      }.bind(this), error);
-    }.bind(this), error);
-    this.pc.ondatachannel = this.onDataChannel.bind(this);
-  },
-  onDataChannel: function ps_onDataChannel(evt) {
-    console.log("Ondatachannel");
-    this.dc = evt.channel;
-    this.dc.onmessage = this.onDataChannelReceive.bind(this);
-    this.dc.onopen = this.onDataChannelOpened.bind(this);
-
-    this.currentstate = 'connected';
-    this.onstatechange && this.onstatechange();
-  },
-  onDataChannelOpened: function ps_onDataChannelOpened(evt) {
-    this.postMessage("I'm Client");
-  },
-  onDataChannelReceive: function ps_onDataChannelReceive(evt){
-    Presentation.log(evt.data);
-  },
-
-  onmessage: null,
-  onstatechange: null,
-  _url: null
+  onpresent: function pss_onopened() {}
 };
 
 Presentation.init();
