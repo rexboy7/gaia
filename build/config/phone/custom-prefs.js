290249f2655d3cbfb1771f7f42926543f3942815
John Hu <johu@mozilla.com>
2015-02-02 16:53:21 +0800

    remove useless prefs

--- build/config/phone/custom-prefs.js
+++ build/config/phone/custom-prefs.js
@@ -3,4 +3,3 @@ user_pref('devtools.responsiveUI.customWidth', 320);
 user_pref('devtools.responsiveUI.customHeight', 480);
 user_pref('devtools.responsiveUI.currentPreset', 'custom');
 user_pref('dom.presentation.enabled', true);
-user_pref('dom.presentation.demo.devices', 'self:127.0.0.1,test:10.10.10.10');
